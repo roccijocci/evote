db9c09d546102df787bfef88fb8cde3bd8d0a781
b3n3b0 <jags4real25@gmail.com>
2018-07-22 01:38:13 +0100

    added a few models

--- polls/views.py
+++ polls/views.py
@@ -33,4 +33,14 @@ def results(request, question_id):
 	return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-	return HttpResponse("You are voting on question %s." % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk = request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		#redisplay the question vting form.
+		return render(request, 'polls/detail.html',{'question':question,'error_message': "You did not select a choice.",})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results', args = (question_id,)))
+	# return HttpResponse("You are voting on question %s." % question_id)
