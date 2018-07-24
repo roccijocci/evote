d4248e3816e99ccbdb34be0af1474cbcc3f92af4
b3n3b0 <jags4real25@gmail.com>
2018-07-24 12:22:43 +0100

    bug fixes| moving forward

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 # from django.template import loader
 from .models import Question, Choice  
 
