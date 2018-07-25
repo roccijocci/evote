0f3fc9976d9a8c40204400fd174305f060952c44
b3n3b0 <jags4real25@gmail.com>
2018-07-25 13:53:59 +0100

    refactoring and finesse

--- polls/views.py
+++ polls/views.py
@@ -25,19 +25,18 @@ class ResultsView(generic.DetailView):
 	model = Question
 	template = 'polls/results.html'
 
-def vote(request, question_id):
-	return Question.objects.order_by('-pub_date')[:5]
-	# question = get_object_or_404(Question, pk=question_id)
-	# try:
-	# 	selected_choice = question.choice_set.get(pk = request.POST['choice'])
-	# except (KeyError, Choice.DoesNotExist):
-	# 	#redisplay the question vting form.
-	# 	return render(request, 'polls/detail.html',{'question':question,'error_message': "You did not select a choice.",})
-	# else:
-	# 	selected_choice.votes += 1
-	# 	selected_choice.save()
-	# 	return HttpResponseRedirect(reverse('polls:results', args = (question_id,)))
-	# 	return HttpResponse("You are voting on question %s." % question_id)
+# def vote(request, question_id):
+# 	question = get_object_or_404(Question, pk=question_id)
+# 	try:
+# 		selected_choice = question.choice_set.get(pk = request.POST['choice'])
+# 	except (KeyError, Choice.DoesNotExist):
+# 		#redisplay the question vting form.
+# 		return render(request, 'polls/detail.html',{'question':question,'error_message': "You did not select a choice.",})
+# 	else:
+# 		selected_choice.votes += 1
+# 		selected_choice.save()
+# 		return HttpResponseRedirect(reverse('polls:results', args = (question_id,)))
+# 		return HttpResponse("You are voting on question %s." % question_id)
 
 
 
@@ -69,15 +68,15 @@ def vote(request, question_id):
 # 	question = get_object_or_404(Question, pk=question_id)
 # 	return render(request, 'polls/results.html', {'question': question})
 
-# def vote(request, question_id):
-# 	question = get_object_or_404(Question, pk=question_id)
-# 	try:
-# 		selected_choice = question.choice_set.get(pk = request.POST['choice'])
-# 	except (KeyError, Choice.DoesNotExist):
-# 		#redisplay the question vting form.
-# 		return render(request, 'polls/detail.html',{'question':question,'error_message': "You did not select a choice.",})
-# 	else:
-# 		selected_choice.votes += 1
-# 		selected_choice.save()
-# 		return HttpResponseRedirect(reverse('polls:results', args = (question_id,)))
-# 	# return HttpResponse("You are voting on question %s." % question_id)
+def vote(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk = request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		#redisplay the question vting form.
+		return render(request, 'polls/detail.html',{'question':question,'error_message': "You did not select a choice.",})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results', args = (question_id,)))
+	# return HttpResponse("You are voting on question %s." % question_id)
