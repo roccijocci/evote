9b17ec1b7b7f217375a5de3301b24a90a2fd4778
b3n3b0 <jags4real25@gmail.com>
2018-07-30 11:42:29 +0100

    1st test passed

--- polls/tests.py
+++ polls/tests.py
@@ -6,9 +6,11 @@ from .models import Question
 # Create your tests here.
 
 class QuestionModelTest(TestCase):
+	def test_was_published_recently_with_future_question(self):
 	# was_published_recently returns true forquestions whose pub_date is in the future
 	#it should return false for pub_date set in the future.
 
-	time = timezone.now() + datetime.timedelta(days=30)
-	future_question = Question(pub_date=time)
-	self.assertIs(future_question.was_published_recently(), false)
+		time = timezone.now() + datetime.timedelta(days=30)
+		future_question = Question(pub_date=time)
+		self.assertIs(future_question.was_published_recently(),
+False)
