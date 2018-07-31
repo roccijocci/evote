3ded2690654ba9973f262485142aca5bcdaf3fd6
b3n3b0 <jags4real25@gmail.com>
2018-07-31 14:31:45 +0100

    Refactoring the views and options
    adding more options to the already available questions to make it more robust ish"

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import (Question)
+from .models import (Question, Choice)
 
 class QuestionAdmin(admin.ModelAdmin):
-	fields = ['pub_date', 'question_text'];
+	# fields = ['pub_date', 'question_text']
+	fieldsets = [
+		(None, {'fields': ['question_text']}),
+		('Date information', {'fields': ['pub_date']})
+	]
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
+class ChoiceInline(admin.StackedInline):
+	model = Choice
+	extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [
+	(None, {'fields': ['question_text']}),
+	('Date information', {'fields':['pub_date'], 'classes': ['collapse']}),
+	]
+	inlines = [ChoiceInline]
+
 admin.site.register(Question, QuestionAdmin)
